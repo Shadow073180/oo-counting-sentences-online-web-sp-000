3934fe21d342f086cff549d231e9de2d87ebf40b
David masterson <david73180@gmail.com>
2018-10-02 23:09:18 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,9 +27,8 @@ class String
   end
 
   def count_sentences
-    new = self.split("."||"?"||"!")
-    new = self.count
-    return new
+    self.split("."||"?"||"!")
+    self.count("."||"?"||"!")
   end
 
 
