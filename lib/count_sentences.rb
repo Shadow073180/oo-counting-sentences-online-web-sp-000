07b034f00f9f41a09abb1c21414c314590c0f054
David masterson <david73180@gmail.com>
2018-10-02 22:48:25 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(.)
+      return true
   end
 
   def question?
-
+    if self.end_with?(?)
+      return true
   end
 
   def exclamation?
-
+    if self.end_with?(!)
+      return true
   end
 
   def count_sentences
-
+    self.split("."||"?"||"!")
   end
 end
