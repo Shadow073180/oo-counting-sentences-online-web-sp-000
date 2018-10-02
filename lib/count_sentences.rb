7e09d41a25db1d524ede483eaafe283aea8e4dec
David masterson <david73180@gmail.com>
2018-10-02 22:49:09 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -5,16 +5,19 @@ class String
   def sentence?
     if self.end_with?(.)
       return true
+    end
   end
 
   def question?
     if self.end_with?(?)
       return true
+    end
   end
 
   def exclamation?
     if self.end_with?(!)
       return true
+    end
   end
 
   def count_sentences
